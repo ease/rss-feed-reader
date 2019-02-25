c5cdcd404882568c7b499d9e1d31bb8fb1485cbf
Denis Salčin <denissa@maestralsolutions.com>
2019-02-25 21:58:42 +0100

    Changing app's name

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to rss-feeder!');
+    expect(page.getTitleText()).toEqual('Welcome to rss-feed-reader!');
   });
 
   afterEach(async () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'rss-feeder';
+  title = 'rss-feed-reader';
 }
